0f2fc05e988e887b0f96c8df7c43960f177afd42
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-14 21:29:07 +0000

    Stockez des citations dans un fichier avec un module json

--- OpenClassrooms/Demarrez-votre-projet-avec-Python/san_antonio.py
+++ OpenClassrooms/Demarrez-votre-projet-avec-Python/san_antonio.py
@@ -2,35 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import random
-
-# Data
-quotes = [
-        "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
-        "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
-]
-
-characters = [
-        "Alvin et les Chipmunks",
-        "Babar",
-        "Betty Boop",
-        "Calimero",
-        "Casper",
-        "Le Chat Potté",
-        "Kirikou"
-]
+import json
 
 # Functions
+
+def read_values_from_json(file, key):
+    values = []
+    with open(file) as f:
+        data = json.load(f)
+        for entry in data:
+            values.append(entry[key])
+    return values
 
 def get_random_item_in(my_list):
     rand_numb = random.randint(0, len(my_list) - 1)
     item = my_list[rand_numb]
     return item
 
+def get_random_character():
+    all_values = read_values_from_json("characters.json", "character")
+    return get_random_item_in(all_values)
+
+def get_random_quote():
+    all_values = read_values_from_json("quotes.json", "quote")
+    return get_random_item_in(all_values)
+
 # Logic
 
 user_answer = "A"
 # Show random quote
 while user_answer != "B":
-    print("{character} a dit : {quote}".format(character=get_random_item_in(characters), quote=get_random_item_in(quotes)))
-    user_answer = "B"
+    user_answer = input("{character} a dit : {quote}".format(character=get_random_character(), quote=get_random_quote()))
 
