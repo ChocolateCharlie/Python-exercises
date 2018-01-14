75ebd936bc0947858d049795132def6dbe446cff
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-14 20:44:43 +0000

    Creez votre premier script

---
+++ OpenClassrooms/Demarrez-votre-projet-avec-Python/san_antonio.py
@@ -0,0 +1,22 @@
+#! usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+# Data
+quotes = [
+        "Ecoutez-moi, Monsieur Shakespeare, nous avons beau etre ou ne pas etre, nous sommes !",
+        "On doit pouvoir choisir entre s'ecouter parler et se faire entendre."
+]
+
+characters = [
+        "Alvin et les Chipmunks",
+        "Babar",
+        "Betty Boop",
+        "Calimero",
+        "Casper",
+        "Le Chat Potte",
+        "Kirikou"
+]
+
+# Logic
+print ("Hello, World !")
+
