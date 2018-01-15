aebf67b7be7df5c077f090ff614b0ff06fa05195
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-15 21:13:19 +0000

    Creez votre premier agent

---
+++ OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
@@ -0,0 +1,18 @@
+#! usr/bin/python3.4
+# -*- coding: utf-8 -*-
+
+##### AGENT #####
+
+class Agent:
+    pass
+
+##### LOGIC #####
+
+def main():
+    first_agent = Agent()
+    second_agent = Agent()
+    print(first_agent, second_agent)
+
+if __name__ == '__main__':
+    main()
+
