2e724a21001fef6545d7496fc863a915ac25b352
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-15 17:27:32 +0000

    Collectez des citations automatiquement avec Scrapy

--- OpenClassrooms/Demarrez-votre-projet-avec-Python/san_antonio.py
+++ OpenClassrooms/Demarrez-votre-projet-avec-Python/san_antonio.py
@@ -4,33 +4,64 @@
 import random
 import json
 
-# Functions
+##### DATA FUNCTIONS #####
 
-def read_values_from_json(file, key):
+# Fetching, cleaning, get random
+
+# Return a list given a JSON file and key
+def read_values_from_json(path, key):
     values = []
-    with open(file) as f:
+    with open(path) as f:
         data = json.load(f)
         for entry in data:
             values.append(entry[key])
-    return values
+        return values
 
-def get_random_item_in(my_list):
+# Clean a given list of strings
+def clean_strings(sentences):
+    cleaned = []
+    for sentence in sentences:
+        cleaned.append(sentence.strip())
+    return cleaned
+
+# Return a random item from a given list
+def random_item_in(my_list):
     rand_numb = random.randint(0, len(my_list) - 1)
-    item = my_list[rand_numb]
-    return item
+    return my_list[rand_numb]
 
-def get_random_character():
-    all_values = read_values_from_json("characters.json", "character")
-    return get_random_item_in(all_values)
+# Return a random value from a given JSON file, given a key
+def random_value(source_path, key):
+    all_values = read_values_from_json(source_path, key)
+    clean_values = clean_strings(all_values)
+    return random_item_in(clean_values)
 
-def get_random_quote():
-    all_values = read_values_from_json("quotes.json", "quote")
-    return get_random_item_in(all_values)
+##### CHARACTERS AND QUOTES #####
 
-# Logic
+# Gather characters from Wikipedia
+def random_character():
+    return random_value("characters.json", "character")
 
-user_answer = "A"
-# Show random quote
-while user_answer != "B":
-    user_answer = input("{character} a dit : {quote}".format(character=get_random_character(), quote=get_random_quote()))
+# Gather quotes from San Antonio
+def random_quote():
+    return random_value("quotes.json", "quote")
+
+# Print a random sentence
+def print_random_sentence():
+    rand_quote = random_quote()
+    rand_character = random_character()
+    print(">>>>> {character} a dit : {quote}".format(character=rand_character, quote=rand_quote))
+
+##### LOGIC #####
+
+def main_loop():
+    while True:
+        print_random_sentence()
+        message = ( "Voulez-vous voir une autre citation ? "
+                    "Pour sortir du programme, tapez [S].")
+        choice = input(message).upper()
+        if choice == 'S':
+            break
+
+if __name__ == '__main__':
+    main_loop()
 
