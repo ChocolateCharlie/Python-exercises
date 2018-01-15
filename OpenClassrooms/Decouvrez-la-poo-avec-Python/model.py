0e038e1720ad6e444b6c2904cf19642721fccd0a
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-15 21:26:55 +0000

    Ajoutez des attributs a partir d'un dictionnaire

--- OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
+++ OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
@@ -1,29 +1,25 @@
 #! usr/bin/python3.4
 # -*- coding: utf-8 -*-
 
-
+import json
 
 ##### AGENT #####
 
 class Agent:
 
     # Constructor
-    def __init__(self, agreeableness):
-        self.agreeableness = agreeableness
-
-    # Greatings function
-    def say_hello(self, first_name):
-        return "Bien le bonjour " + first_name + " !"
-
+    def __init__(self, **agent_attributes):
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
 
 
 
 ##### LOGIC #####
 
 def main():
-    agent = Agent(23)
-    print(agent.say_hello("Charlie"))
-    print(agent.agreeableness)
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
 
 if __name__ == '__main__':
     main()
