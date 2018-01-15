79e809d6642486469657179886540ea0c7810dde
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-15 21:20:02 +0000

    Customisez votre agent

--- OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
+++ OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
@@ -1,17 +1,29 @@
 #! usr/bin/python3.4
 # -*- coding: utf-8 -*-
 
+
+
 ##### AGENT #####
 
 class Agent:
-    pass
+
+    # Constructor
+    def __init__(self, agreeableness):
+        self.agreeableness = agreeableness
+
+    # Greatings function
+    def say_hello(self, first_name):
+        return "Bien le bonjour " + first_name + " !"
+
+
+
 
 ##### LOGIC #####
 
 def main():
-    first_agent = Agent()
-    second_agent = Agent()
-    print(first_agent, second_agent)
+    agent = Agent(23)
+    print(agent.say_hello("Charlie"))
+    print(agent.agreeableness)
 
 if __name__ == '__main__':
     main()
