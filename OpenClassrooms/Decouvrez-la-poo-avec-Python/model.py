cac7ef06e4dcb9285bcaf2d634bd141276e29331
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-16 17:59:51 +0000

    Creez votre premiere Zone

--- OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
+++ OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
@@ -4,6 +4,41 @@
 import json
 import math
 
+##### ZONE #####
+
+class Zone:
+    
+    # Attributes
+    # Grid size values
+    MIN_LATITUDE_DEGREES = -90
+    MAX_LATITUDE_DEGREES = 90
+    MIN_LONGITUDE_DEGREES = -180
+    MAX_LONGITUDE_DEGREES = 180
+    # Zone size
+    WIDTH_DEGREES = 1
+    HEIGHT_DEGREES = 1
+    # List of zones
+    ZONES = []
+
+    # Constructor
+    def __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.inhabitants = 0
+
+    # Initialize the list of zones
+    @classmethod
+    def initialize_zones(cls):
+        for latitude in range(cls.MIN_LATITUDE_DEGREES, cls.MAX_LATITUDE_DEGREES, cls.HEIGHT_DEGREES):
+            for longitude in range(cls.MIN_LONGITUDE_DEGREES, cls.MAX_LONGITUDE_DEGREES, cls.WIDTH_DEGREES):
+                bottom_left_corner = Position(longitude, latitude)
+                top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
+                zone = Zone(bottom_left_corner, top_right_corner)
+                cls.ZONES.append(zone)
+        print(len(cls.ZONES))
+
+
+
 ##### POSITION #####
 
 class Position:
@@ -45,8 +80,7 @@ def main():
         longitude = agent_attributes.pop("longitude")
         position = Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-        print(agent.position.longitude)
-        print(agent.position.latitude)
+    Zone.initialize_zones()
 
 if __name__ == '__main__':
     main()
