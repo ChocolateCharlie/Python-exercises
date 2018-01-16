763ea60d501dab306b79052a06f5db658de0b02a
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-16 18:26:18 +0000

    Ajoutez un habitant

--- OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
+++ OpenClassrooms/Decouvrez-la-poo-avec-Python/model.py
@@ -24,7 +24,12 @@ class Zone:
     def __init__(self, corner1, corner2):
         self.corner1 = corner1
         self.corner2 = corner2
-        self.inhabitants = 0
+        self.inhabitants = []
+
+    # The population is the number of inhabitants
+    @property
+    def population(self):
+        return len(self.inhabitants)
 
     # Initialize the list of zones
     @classmethod
@@ -35,7 +40,29 @@ class Zone:
                 top_right_corner = Position(longitude + cls.WIDTH_DEGREES, latitude + cls.HEIGHT_DEGREES)
                 zone = Zone(bottom_left_corner, top_right_corner)
                 cls.ZONES.append(zone)
-        print(len(cls.ZONES))
+
+    # Compute the index of the ZONES array that contains a given position
+    @classmethod
+    def find_zone_that_contains(cls, position):
+        longitude_index = int((position.longitude_degrees - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        latitude_index = int((position.latitude_degrees - cls.MIN_LATITUDE_DEGREES) / cls.HEIGHT_DEGREES)
+        longitude_bins = int((cls.MAX_LONGITUDE_DEGREES - cls.MIN_LONGITUDE_DEGREES) / cls.WIDTH_DEGREES)
+        zone_index = latitude_index * longitude_bins + longitude_index
+        # Check the zoning is correct before returning zone
+        zone = cls.ZONES[zone_index]
+        assert zone.contains(position)
+        return zone
+
+    # Test for a given position if it belongs to the zone
+    def contains(self, position):
+        return  position.longitude >= min(self.corner1.longitude, self.corner2.longitude) and \
+                position.longitude <= max(self.corner1.longitude, self.corner2.longitude) and \
+                position.latitude >= min(self.corner1.latitude, self.corner2.latitude) and \
+                position.latitude <= max(self.corner2.latitude, self.corner2.latitude)
+    
+    # Add an inhabitant to the zone
+    def add_inhabitant(self, inhabitant):
+        self.inhabitants.append(inhabitant)
 
 
 
@@ -75,12 +102,15 @@ class Agent:
 ##### LOGIC #####
 
 def main():
+    Zone.initialize_zones()
     for agent_attributes in json.load(open("agents-100k.json")):
         latitude = agent_attributes.pop("latitude")
         longitude = agent_attributes.pop("longitude")
         position = Position(longitude, latitude)
         agent = Agent(position, **agent_attributes)
-    Zone.initialize_zones()
+        zone = Zone.find_zone_that_contains(position)
+        zone.add_inhabitant(agent)
+        print("Zone population: ", zone.population)
 
 if __name__ == '__main__':
     main()
