c2711a9f32a10a34abb32096f43300267aee919d
ChocolateCharlie <ChocolateCharlie@users.noreply.github.com>
2018-01-18 21:45:03 +0100

    Ajout des accents

--- OpenClassrooms/Perfectionnez-vous-en-Python/karadoc.py
+++ OpenClassrooms/Perfectionnez-vous-en-Python/karadoc.py
@@ -4,7 +4,7 @@
 
 
 if __name__ == "__main__":
-    print("Les chicots, c'est sacr{ ! Parce que si j'les lave pas maintenant, dans dix ans, c'est tout a la soupe. Et l'mec qui me fera manger de la soupe il est pas ne !")
+    print("Les chicots, c'est sacré ! Parce que si j'les lave pas maintenant, dans dix ans, c'est tout à la soupe. Et l'mec qui me fera manger de la soupe il est pas né !")
 else:
     print("Du passe faisons table de marbre.")
 
