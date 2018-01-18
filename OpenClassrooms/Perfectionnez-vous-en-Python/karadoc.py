e2dd5669ab538f60d8e8310898024ab57375e34f
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-18 20:43:36 +0000

    Organisez un script

---
+++ OpenClassrooms/Perfectionnez-vous-en-Python/karadoc.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+
+
+if __name__ == "__main__":
+    print("Les chicots, c'est sacr{ ! Parce que si j'les lave pas maintenant, dans dix ans, c'est tout a la soupe. Et l'mec qui me fera manger de la soupe il est pas ne !")
+else:
+    print("Du passe faisons table de marbre.")
+
