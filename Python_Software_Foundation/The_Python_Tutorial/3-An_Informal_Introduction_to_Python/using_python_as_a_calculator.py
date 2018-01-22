53689272f49326552dbd939f82cc3f11d0ba63bc
chocolatecharlie <aurore.amrit@gmail.com>
2018-01-22 20:37:55 +0000

    Create An Informal Introduction to Python

---
+++ Python_Software_Foundation/The_Python_Tutorial/3-An_Informal_Introduction_to_Python/using_python_as_a_calculator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+
+################################################################################
+################################ NUMBERS #######################################
+################################################################################
+
+print("----- Simple operations -----")
+print(2 + 2)
+print(50 - 5 * 6)
+print((50 - 5 * 6) / 4)
+print (8 / 5) # division always returns a floating point number
+
+print("----- Divisions and remainders -----")
+print(17 / 3) # classic division returns a float
+print(17 // 3) # floor division discards the fractional part
+print(17 % 3) # modulo
+
+print("----- Powers -----")
+print(5 ** 2)
+print(2 ** 7)
+
+print("----- Equal sign -----")
+width = 20
+height = 5 * 9
+print(width * height)
+
+print("----- Mixed type operands -----")
+print(4 * 3.75 - 1)
+
+################################################################################
+################################# STRINGS ######################################
+################################################################################
+
